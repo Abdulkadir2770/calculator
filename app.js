fde82e48dee98f19e45a2c8d9bef3f25e5d3ec16
Amadi Y <absnacks17@gmail.com>
2022-08-07 13:02:16 -0500

    made additions to script.js

--- app.js
+++ app.js
@@ -4,21 +4,38 @@ const numbers = document.querySelectorAll(".number");
 const operators = document.querySelectorAll(".operator");
 const clearBtn = document.querySelector(".clear");
 
-// operatoration 
+// operators 
 const add = () => {
-
+    return "+";
 }
 
 const subtract = () => {
-
+    return "-";
 }
 
 const multiply = () => {
-
+    return "*";
 }
 const divide = () => {
-
+    return "/";
 }
+
+// operation
+const operation = (operator, num1, num2) => {
+    
+}
+
+const populateDisplay = () => {
+    let displayValue;
+
+    numbers.forEach((number) => {
+        number.addEventListener("click", (e) => {
+            display.textContent = e.target.innerText;
+        })
+    })
+}
+
+populateDisplay();
 
 // a function that evaluates expressions & displays the results on the screen
 const evaluate = () => {
