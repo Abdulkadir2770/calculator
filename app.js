818a981b1cd3384b0c51dd2caee039c4a5142a55
Amadi Y <absnacks17@gmail.com>
2022-08-07 12:03:45 -0500

    retrieved all the elements from the html file

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
 // retrieved elements
+const display = document.querySelector("#display-field");
+const numbers = document.querySelectorAll(".number");
+const operators = document.querySelectorAll(".operator");
+const clearBtn = document.querySelector(".clear");
+console.log(clearBtn);
 
-/* project objectives / goals
-    - must have a function that evaluates expressions
-*/
-    
+// a function that evaluates expressions & displays the results on the screen
+const evaluate = () => {
+    // figure out how to turn a string char into a number
+
+    // reject none number characters
+}
